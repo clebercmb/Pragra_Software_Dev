792bdfde1dd2bb278cb2a73bc6a6f38809143290
Cleber Miranda Barbosa <cleber@gmail.com>
2021-06-28 19:49:06 -0400

    Session HashMap

--- session_20210626/src/main/java/App.java
+++ session_20210626/src/main/java/App.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) {
@@ -10,8 +10,31 @@ public class App {
 
         Arrays.stream(text.split("")).forEach( p-> map.compute(p, (k,v)  -> v==null? 1 : v+1 ))  ;
 
-        System.out.println(map);
+        //map.computeIfPresent()
 
+        System.out.println("======>>>>>>");
+        System.out.println(map);
+        Map<String, Integer> map2 = new HashMap<>();
+        String[] strings = {"a", "b", "a", "b"};
+        word0(map2, strings);
+        System.out.println(map2);
+
+    }
+
+    public static Map<String, Integer> word0(Map<String, Integer> map, String[] strings) {
+        Arrays.stream(strings).forEach(c-> map.put(c,0));
+        return map;
+    }
+
+
+    public Map<String, String> mapShare(Map<String, String> map) {
+
+        if( map.containsKey("a") ) {
+            map.put("b", map.get("a"));
+        }
+
+        map.remove( "c");
+        return map;
     }
 
 }
